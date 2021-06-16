93abfcbdd5db0494ac178b2bf5a4d61b60b9dd89
uno-33 <romandefault@gmail.com>
2021-06-16 18:09:24 +0300

    Add Event Aggregation in WPF

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using RMDesktopUI.EventModels;
 using RMDesktopUI.Library.Api;
 using System;
 using System.Collections.Generic;
@@ -13,10 +14,12 @@ namespace RMDesktopUI.ViewModels
         private string _userName;
         private string _password;
         private IAPIHelper _apiHelper;
+        private IEventAggregator _events;
 
-        public LoginViewModel(IAPIHelper apiHelper)
+        public LoginViewModel(IAPIHelper apiHelper, IEventAggregator events)
         {
             _apiHelper = apiHelper;
+            _events = events;
         }
 
         public string UserName
@@ -95,6 +98,8 @@ namespace RMDesktopUI.ViewModels
 
                 // Capture more info about user
                 await _apiHelper.GetLoggedInUserInfo(result.Access_Token);
+
+                await _events.PublishOnUIThreadAsync(new LogOnEvent());
             }
             catch(Exception ex)
             {
