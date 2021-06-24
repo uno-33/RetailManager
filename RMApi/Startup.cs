d45de91aa615f23a1eb8f19996dfbebd0da9ede1
uno-33 <romandefault@gmail.com>
2021-06-24 14:22:16 +0300

    Moved Secure Key to Cinfiguration

--- RMApi/Startup.cs
+++ RMApi/Startup.cs
@@ -59,7 +59,7 @@ namespace RMApi
                     jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecret")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ValidateLifetime = true,
